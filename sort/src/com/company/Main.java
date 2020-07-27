c1a82af3242715199dd2f410c602675ddac3bd84
borgesaires <68474067+borgesaires@users.noreply.github.com>
2020-07-27 16:04:47 +0800

    Update Main.java

--- sort/src/com/company/Main.java
+++ sort/src/com/company/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int arr[] = {1, 9, 8, 7, 6, 5, 4, 3, 2};
+        int arr[] = {1, 9, 8, 7, 6, 5, 4, 3, 2, 109};
 
         Sort T = new SortTest();
         //T.quickSort(arr);
